617c4aeefaae128ffe665f0814650adeb6bf1746
Petrov Aleksei <alexey.v.petrov@kaspersky.com>
2020-07-24 00:01:41 +0300

    Basic skeleton for filesystem

--- src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
+++ src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -1,5 +1,22 @@
 package com.rtjvm.scala.oop.filesystem
 
-object Filesystem extends App {
+import java.util.Scanner
 
+import com.rtjvm.scala.oop.commands.Command
+import com.rtjvm.scala.oop.files.Directory
+
+object Filesystem extends App {
+  val root = Directory.ROOT
+  val scanner = new Scanner(System.in)
+
+  var state = State(root, root)
+
+  while(true) {
+    state.show()
+    val input = scanner.nextLine()
+    state = Command.from(input).apply(state)
+    // eq to
+    // val command = Command.from(input)
+    // state = command(state)
+  }
 }
