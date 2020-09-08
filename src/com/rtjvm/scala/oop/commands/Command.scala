88cbdc8dbf5920c9667ad1f4c484acd8ffd613ee
Petrov Aleksei <alexey.v.petrov@kaspersky.com>
2020-09-08 13:48:31 +0300

    Add commands parsing

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -7,6 +7,24 @@ trait Command {
 }
 
 object Command {
-  def from(input: String): Command =
-    new UnknownCommand
+  val MKDIR = "mkdir"
+
+  def emptyCommand: Command = new Command {
+    override def apply(state: State): State = state
+  }
+
+  def incompleteCommand(name: String): Command = new Command {
+    override def apply(state: State): State = state setMessage s"$name: incomplete command"
+  }
+
+  def from(input: String): Command = {
+    val tokens: Array[String] = input.split(' ')
+
+    if (input.isEmpty || tokens.isEmpty) emptyCommand
+    else if (MKDIR.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(MKDIR)
+      else new Mkdir(tokens(1))
+    }
+    else new UnknownCommand
+  }
 }
