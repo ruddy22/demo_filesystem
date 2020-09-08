49623d7c3242b2118826b0fe97700d86d8d4ce74
Petrov Aleksei <alexey.v.petrov@kaspersky.com>
2020-09-08 13:49:26 +0300

    Add mkdir command

--- src/com/rtjvm/scala/oop/files/DirEntry.scala
+++ src/com/rtjvm/scala/oop/files/DirEntry.scala
@@ -1,5 +1,7 @@
 package com.rtjvm.scala.oop.files
 
 abstract class DirEntry(val parentPath: String, val name: String) {
+  def path: String = parentPath + Directory.SEPARATOR + name
 
+  def asDirectory: Directory
 }
--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -3,6 +3,19 @@ package com.rtjvm.scala.oop.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
+  def hasEntry(entry: String): Boolean = ???
+
+  def getAllFoldersInPath: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = ???
+
+  def replaceEntry(entryName: String, directory: Directory): Directory = ???
+
+  def asDirectory: Directory = this
 }
 
 object Directory {
