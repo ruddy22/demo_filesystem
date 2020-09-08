ca579cf04034e38138de3d184e53da0966ea101d
Petrov Aleksei <alexey.v.petrov@kaspersky.com>
2020-09-08 16:26:32 +0300

    Finish mkdir and add ls command

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -8,6 +8,7 @@ trait Command {
 
 object Command {
   val MKDIR = "mkdir"
+  val LS = "ls"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -24,6 +25,9 @@ object Command {
     else if (MKDIR.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(MKDIR)
       else new Mkdir(tokens(1))
+    }
+    else if (LS.equals(tokens(0))) {
+      new Ls
     }
     else new UnknownCommand
   }
